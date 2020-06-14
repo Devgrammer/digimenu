8f4c5a3ecdd9e434b527f5c36d53e85a6402fa4c
Abhinav Rastogi <rastogi.abhinav8@gmail.com>
2020-06-15 03:13:58 +0530

    Whatsapp Chat Testing

--- src/App.js
+++ src/App.js
@@ -26,7 +26,7 @@ function App() {
           </div>
         </Row>
         <Row>
-          <ChatBot />
+          {/* <ChatBot /> */}
         </Row>
         <Row></Row>
         <Row className="justify-content-center">
